94876055da4a396388b5a7ae7ae1f203a4e677dc
Heysanti07 <sanbeltranchuggo@gmail.com>
2024-11-25 19:35:49 -0600

    se hizo el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,32 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0">Bienvenidos</h4>
+
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Asesor</a></li>
+                    <li class="breadcrumb-item active">Calendario</li>
+                </ol>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="mb-sm-0">Bienvendo, {{ Auth::user()->nombre }} {{ Auth::user()->apellido }}</h4>
+                <br>
+                <h4 class="mb-sm-0">Es un placer tener de vuelta.</h4>
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
+
